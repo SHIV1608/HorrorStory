5e944451969f85066421025ece4f859df0c3f712
SHIV1608 <35663065+SHIV1608@users.noreply.github.com>
2019-01-03 23:42:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shiva/horrorstory/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.shiva.horrorstory;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_home);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openlist();
+            }
+        });
+    }
+    public void openlist() {
+        Intent intent = new Intent(this, list.class);
+        startActivity(intent);
+    }
+}
